165276910e325d2b25227b59e0babfded5ddcc36
Redwan468001 <redwanshuvo1@gmail.com>
2025-01-22 19:33:49 +0600

    Post

--- ArtAndLiterature/views.py
+++ ArtAndLiterature/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from . models import ArtAndLiterature
 
 # Create your views here.
+def artandliterature(request):
+    posts = ArtAndLiterature.objects.all()
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'index.html', context)
+
--- config/views.py
+++ config/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, redirect
+from ArtAndLiterature.models import ArtAndLiterature
 
 # Create your views here.
 def home(request):
-    title = 'Test run'
+    posts = ArtAndLiterature.objects.all().order_by('-create_at')
+    stories = []
+    novels = []
+    for post in posts:
+        if post.category.name == 'Novel':
+            novels.append(post)
+        elif post.category.name == 'Story':
+            stories.append(post)
+
     context = {
-        'title': title,
+        'posts': posts,
+        'novels': novels,
+        'stories': stories,
     }
     return render(request, 'index.html', context)
